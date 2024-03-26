43475da57b80e8b6a5becd5d403905e9b55b0de9
ignasrum <rumbavicius@gmail.com>
2024-03-26 23:23:36 +0100

    Update Styri Hoenir keymaps

--- keyboards/styri/hoenir/keymaps/default/keymap.c
+++ keyboards/styri/hoenir/keymaps/default/keymap.c
@@ -16,14 +16,14 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    BASE,  // default layer / FIRST
-    ND,    // SECOND
-    RD,    // THIRD
-    TH,    // FOURTH
+    ST, // default layer / FIRST, used for Windows/Linux
+    ND, // SECOND, used for Windows/Linux
+    RD, // THIRD, used for Mac
+    TH, // FOURTH, used for Mac
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap BASE: Default layer
+/* Keymap ST: Default layer (Windows/Linux base layer)
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | Esc    |   1  |   2  |   3  |   4  |   5  |RGBTOG|           |RGBVAI|   6  |   7  |   8  |   9  |   0  |   -    |
@@ -37,33 +37,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Ctrl   |      | Del  | Left | Right|                                       |  Up  | Down |   [  |   ]  | Ctrl   |
  * `------------------------------------'                                       `------------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | Alt  | LGui |       | PgUp | Alt  |
+ *                                        | LGui | Home |       | PgUp | Alt  |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      | Home |       | PgDn |      |      |
- *                                 | Space| Ctrl |------|       |------| Enter| Back-|
- *                                 |      |      | End  |       | L2   |      | space|
+ *                                 |      |      | End  |       | PgDn |      |      |
+ *                                 | Space| Alt  |------|       |------| Enter| Back-|
+ *                                 |      |      | Ctrl |       | L2   |      | space|
  *                                 `--------------------'       `--------------------'
  */
-[BASE] = LAYOUT(
+[ST] = LAYOUT(
     KC_ESC,   KC_1,   KC_2,    KC_3,     KC_4,      KC_5,      RGB_TOG,        RGB_VAI,  KC_6,     KC_7,   KC_8,     KC_9,     KC_0,     KC_MINS,
     KC_TAB,   KC_Q,   KC_W,    KC_E,     KC_R,      KC_T,      RGB_MOD,        RGB_VAD,  KC_Y,     KC_U,   KC_I,     KC_O,     KC_P,     KC_BSLS,
     KC_CAPS,  KC_A,   KC_S,    KC_D,     KC_F,      KC_G,      RGB_SAI,        RGB_HUI,  KC_H,     KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOTE,
     KC_LSFT,  KC_Z,   KC_X,    KC_C,     KC_V,      KC_B,                                KC_N,     KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
     KC_LCTL,  KC_NO,  KC_DEL,  KC_LEFT,  KC_RGHT,                                                  KC_UP,  KC_DOWN,  KC_LBRC,  KC_RBRC,  KC_RCTL,
-                                                    KC_LALT,   KC_LGUI,        KC_PGUP,  KC_RALT,
-                                                               KC_HOME,        KC_PGDN,
-                                         KC_SPACE,  KC_LCTL,   KC_END,         MO(ND),   KC_ENT,   KC_BSPC
+                                                    KC_LGUI,   KC_HOME,        KC_PGUP,  KC_RALT,
+                                                               KC_END,         KC_PGDN,
+                                         KC_SPACE,  KC_LALT,   KC_LCTL,        MO(ND),   KC_ENT,   KC_BSPC
     ),
-/* Keymap ND: Second layer
+/* Keymap ND: Second layer (Windows/Linux second layer)
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        | F1   | F2   | F3   | F4   | F5   |      |           |      | F6   | F7   | F8   | F9   | F10  | F11    |
+ * |        | F1   | F2   | F3   | F4   | F5   |      |           | L3   | F6   | F7   | F8   | F9   | F10  | F11    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      | F12    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |------|           |------|      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |      |           |      |      |      | <    | >    |      |        |
  * |--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------|
  * |        |      |      |      |      |                                       |      |      |      |      |        |
  * `------------------------------------'                                       `------------------------------------'
@@ -76,56 +76,56 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `----------------------'
  */
 [ND] = LAYOUT(
-    KC_NO,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_NO,      KC_NO,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_F12,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                                      KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,
+    KC_NO,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_NO,      TG(RD), KC_F6,  KC_F7,  KC_F8,   KC_F9,      KC_F10,  KC_F11,
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,      KC_NO,   KC_F12,
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,      KC_NO,   KC_NO,
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                      KC_NO,  KC_NO,  KC_NUBS, S(KC_NUBS), KC_NO,   KC_NO,
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                                      KC_NO,  KC_NO,   KC_NO,      KC_NO,   KC_NO,
                                             KC_NO,  KC_NO,      KC_NO,  KC_NO,
                                                     KC_NO,      KC_NO,
                                     KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO, KC_NO
     ),
-/* Keymap RD: Third layer
+/* Keymap RD: Third layer (Mac base layer)
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * | Esc    |   1  |   2  |   3  |   4  |   5  |RGBTOG|           |RGBVAI|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |RGBMOD|           |RGBVAD|   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |------|           |------|      |      |      |      |      |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * | Caps   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |--------+------+------+------+------+------|RGBSAI|           |RGBHUI|------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * |--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------|
- * |        |      |      |      |      |                                       |      |      |      |      |        |
+ * | Ctrl   |      | Del  | Left | Right|                                       |  Up  | Down |   [  |   ]  | Ctrl   |
  * `------------------------------------'                                       `------------------------------------'
- *                                        ,-------------.       ,---------------.
- *                                        |      |      |       |      |        |
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      |      |       |      |        |      |
- *                                 |      |      |------|       |------|        |      |
- *                                 |      |      |      |       |      |        |      |
- *                                 `--------------------'       `----------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        | Alt  | Ctrl |       | PgUp | Alt  |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      | Home |       | PgDn |      |      |
+ *                                 | Space| LGui |------|       |------| Enter| Back-|
+ *                                 |      |      | End  |       | L4   |      | space|
+ *                                 `--------------------'       `--------------------'
  */
 [RD] = LAYOUT(
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                                      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-                                            KC_NO,  KC_NO,      KC_NO,  KC_NO,
-                                                    KC_NO,      KC_NO,
-                                    KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO, KC_NO
+    KC_ESC,   KC_1,   KC_2,    KC_3,     KC_4,      KC_5,      RGB_TOG,        RGB_VAI,  KC_6,     KC_7,   KC_8,     KC_9,     KC_0,     KC_MINS,
+    KC_TAB,   KC_Q,   KC_W,    KC_E,     KC_R,      KC_T,      RGB_MOD,        RGB_VAD,  KC_Y,     KC_U,   KC_I,     KC_O,     KC_P,     KC_BSLS,
+    KC_CAPS,  KC_A,   KC_S,    KC_D,     KC_F,      KC_G,      RGB_SAI,        RGB_HUI,  KC_H,     KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOTE,
+    KC_LSFT,  KC_Z,   KC_X,    KC_C,     KC_V,      KC_B,                                KC_N,     KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+    KC_LCTL,  KC_NO,  KC_DEL,  KC_LEFT,  KC_RGHT,                                                  KC_UP,  KC_DOWN,  KC_BSLS,  KC_RBRC,  KC_RCTL,
+                                                    KC_LALT,   KC_LCTL,        KC_PGUP,  KC_RALT,
+                                                               KC_HOME,        KC_PGDN,
+                                         KC_SPACE,  KC_LGUI,   KC_END,         MO(TH),   KC_ENT,   KC_BSPC
     ),
-/* Keymap TH: Fourth layer
+/* Keymap TH: Fourth layer (Mac second layer)
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        | F1   | F2   | F3   | F4   | F5   |      |           | L1   | F6   | F7   | F8   | F9   | F10  | F11    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      | F12    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |------|           |------|      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |      |           |      |      |      | <    | >    |      |        |
  * |--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------|
  * |        |      |      |      |      |                                       |      |      |      |      |        |
  * `------------------------------------'                                       `------------------------------------'
@@ -138,11 +138,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `----------------------'
  */
 [TH] = LAYOUT(
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                                      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+    KC_NO,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_NO,      TG(RD), KC_F6,  KC_F7,  KC_F8,   KC_F9,      KC_F10,  KC_F11,
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,      KC_NO,   KC_F12,
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,      KC_NO,   KC_NO,
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                      KC_NO,  KC_NO,  KC_NUBS, S(KC_NUBS), KC_NO,   KC_NO,
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                                      KC_NO,  KC_NO,   KC_NO,      KC_NO,   KC_NO,
                                             KC_NO,  KC_NO,      KC_NO,  KC_NO,
                                                     KC_NO,      KC_NO,
                                     KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO, KC_NO
